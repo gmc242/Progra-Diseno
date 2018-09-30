01dc53ded6014fb3f71f315ba561e74424d47abe
Gerardo Monge Corella <g.m.c_242@hotmail.com>
2018-09-30 12:43:46 -0600

    Cambios de formato a DAOPdf y DAOTxt.

--- src/Vista/admin/Admin.java
+++ src/Vista/admin/Admin.java
@@ -1,4 +1,31 @@
 package Vista.admin;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Admin {
+
+    @FXML private ComboBox alfabetosCombo;
+
+    @FXML private TextField alfabetoNombre;
+
+    @FXML private TextArea alfabetoSimbolos;
+
+    @FXML public void cargarInfo(){}
+
+    @FXML public void cargarSimbolos(){}
+
+    @FXML public void agregarAlfabeto(){}
+
+    @FXML public void eliminarAlfabeto(){}
+
+    @FXML public void modificarAlfabeto(){}
+
+    @FXML public void refrescarAlgoritmos(){}
+
+    @FXML public void cargarAlgoritmo(){}
+
+    @FXML public void eliminarAlgoritmo(){}
 }
